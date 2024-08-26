120e1d7d2038c3c532257e69d7323fb481319964
nagasritha <eernagasritha@gmail.com>
2024-08-26 23:23:15 +0530

    books crud operations

--- Mongodb-assaignment-20/src/books-cli.js
+++ Mongodb-assaignment-20/src/books-cli.js
@@ -1,29 +1,85 @@
+// const CLI = require('./cli.js');
+// const { getAllBooks, 
+//         getAllBooksByAuthor, 
+//         getAllBooksByAuthorCaseInsensitive , 
+//         addNewBook, 
+//         deleteBookByTitle, 
+//         updateTitle,
+//         getBooksByTitle} = require('./books.js');
+// const { disconnect } = require('./connection.js');
+
+// const app = new CLI("Books' CLI", { close: disconnect });
+
+// // Add command to get all books
+// app.addCommand(getAllBooks, 'books', 'Get a list of books');
+
+// // Add command to get books that match author name approximately
+// app.addCommand(getAllBooksByAuthor, 'get', 'Get books that match author name approximately');
+
+// app.addCommand(getAllBooksByAuthorCaseInsensitive,'getByAuthor','retreive the books written by given author');
+
+// app.addCommand(addNewBook, 'add','addes new Item in to the existing books collection');
+
+// app.addCommand(deleteBookByTitle, 'delete', 'Delete books by Title');
+
+// app.addCommand(updateTitle,'update','updates the book old title with new Title');
+
+// app.addCommand(getBooksByTitle,'getBookByTitle', 'retreives the bok that matches given title');
+
+// app.run();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const CLI = require('./cli.js');
-const { getAllBooks, 
-        getAllBooksByAuthor, 
-        getAllBooksByAuthorCaseInsensitive , 
-        addNewBook, 
-        deleteBookByTitle, 
-        updateTitle,
-        getBooksByTitle} = require('./books.js');
+const {
+    getAllBooks,
+    getAllBooksByAuthor,
+    getAllBooksByAuthorCaseInsensitive,
+    addNewBook,
+    deleteBookByTitle,
+    updateTitle,
+    getBooksByTitle
+} = require('./books.js');
 const { disconnect } = require('./connection.js');
 
 const app = new CLI("Books' CLI", { close: disconnect });
 
-// Add command to get all books
-app.addCommand(getAllBooks, 'books', 'Get a list of books');
+// Command to get all books
+app.addCommand(getAllBooks, 'books', 'Get a list of all books');
 
-// Add command to get books that match author name approximately
-app.addCommand(getAllBooksByAuthor, 'get', 'Get books that match author name approximately');
+// Command to get books by author (case-sensitive)
+app.addCommand(getAllBooksByAuthor, 'getByAuthorWithPerfectMatch', 'Get books by a specific author (case-sensitive)');
 
-app.addCommand(getAllBooksByAuthorCaseInsensitive,'getByAuthor','retreive the books written by given author');
+// Command to get books by author (case-insensitive)
+app.addCommand(getAllBooksByAuthorCaseInsensitive, 'getByAuthor', 'Retrieve books written by the given author (case-insensitive)');
 
-app.addCommand(addNewBook, 'add','addes new Item in to the existing books collection');
+// Command to add a new book
+app.addCommand(addNewBook, 'add', 'Add a new book to the collection');
 
-app.addCommand(deleteBookByTitle, 'delete', 'Delete books by Title');
+// Command to delete a book by title
+app.addCommand(deleteBookByTitle, 'deletebyTitle', 'Delete a book by its title');
 
-app.addCommand(updateTitle,'update','updates the book old title with new Title');
+// Command to update a book's title
+app.addCommand(updateTitle, 'updateTitle', 'Update the title of a book');
 
-app.addCommand(getBooksByTitle,'getBookByTitle', 'retreives the bok that matches given title');
+// Command to get a book by title
+app.addCommand(getBooksByTitle, 'getBookByTitle', 'Retrieve a book by its title');
 
 app.run();
--- Mongodb-assaignment-20/src/books.js
+++ Mongodb-assaignment-20/src/books.js
@@ -1,75 +1,175 @@
+// const { connect, disconnect } = require('./connection');
+
+// const url = 'mongodb://localhost/';
+// async function connection(){
+//     const connection  = await connect(url);
+//     const db = connection.db('g7_cr_20240826');
+//     const booksCollection = db.collection('books');
+//     return booksCollection;
+// }
+
+
+// async function getAllBooks() {
+//     let booksCollection = await connection();
+    
+//     const books = await booksCollection.find({}).toArray();
+//     return books;
+// }
+
+// async function getAllBooksByAuthor(author){
+//     let connection = await connect(url);
+//     let db = connection.db('g7_cr_20240826');
+//     let booksCollection = db.collection('books');
+//     let result = await booksCollection.findOne({author});
+//     return result;
+// }
+
+// async function getAllBooksByAuthorCaseInsensitive(author) {
+//     let connection = await connect(url);
+//     let db = connection.db('g7_cr_20240826');
+//     let booksCollection = db.collection('books');
+//     const book = await booksCollection.findOne({ author: new RegExp(author, 'i') });
+//     return book;
+// }
+
+// async function addNewBook(title,isbn,author,price,description,tags,cover,authorId,reviews){
+//     let newBook = {title,isbn,author,price,description,tags,cover,authorId,reviews};
+//     const connection = await connect(url);
+//     const db = connection.db('g7_cr_20240826');
+//     const booksCollection = db.collection('books');
+//     const result = await booksCollection.insertOne(newBook);
+//     return result.insertedId;
+// }
+
+// async function deleteBookByTitle(title){
+//     const connection = await connect(url);
+//     const db = connection.db('g7_cr_20240826');
+//     const booksCollection = db.collection('books');
+//     const book =await booksCollection.deleteOne({title});
+//     return "book with title \""+title+"\" deleted"
+// }
+
+// async function updateTitle(title,newTitle){
+//     const connection = await connect(url);
+//     const db = connection.db('g7_cr_20240826');
+//     const booksCollection = db.collection('books');
+//     const result = await booksCollection.updateMany({title},
+//                                                     {$set : {title : newTitle}}
+//                                                     );
+//     if(result.matchedCount > 0){
+//         return ("sucsessfully updates");
+//     }else{
+//         return ("No bok Found with iven title");
+//     }
+// }
+
+// async function getBooksByTitle(title){
+//     //let booksCollection = connection();
+//     const connection  = await connect(url);
+//     const db = connection.db('g7_cr_20240826');
+//     const booksCollection = db.collection('books');
+//     let result =await booksCollection.findOne({title});
+//     return result;
+// }
+
+// module.exports = {
+//     getAllBooks,
+//     getAllBooksByAuthor,
+//     getAllBooksByAuthorCaseInsensitive,
+//     addNewBook,
+//     deleteBookByTitle,
+//     updateTitle,
+//     getBooksByTitle
+// };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const { connect, disconnect } = require('./connection');
 
 const url = 'mongodb://localhost/';
-async function connection(){
-    const connection  = await connect(url);
-    const db = connection.db('g7_cr_20240826');
-    const booksCollection = db.collection('books');
-    return booksCollection;
+const dbName = 'g7_cr_20240826';
+const collectionName = 'books';
+
+// Function to connect to the database and get the books collection
+async function getBooksCollection() {
+    const connection = await connect(url);
+    const db = connection.db(dbName);
+    return db.collection(collectionName);
 }
 
-
+// Function to get all books
 async function getAllBooks() {
-    let booksCollection = await connection();
-    
+    const booksCollection = await getBooksCollection();
     const books = await booksCollection.find({}).toArray();
     return books;
 }
 
-async function getAllBooksByAuthor(author){
-    let connection = await connect(url);
-    let db = connection.db('g7_cr_20240826');
-    let booksCollection = db.collection('books');
-    let result = await booksCollection.findOne({author});
-    return result;
+// Function to get books by author (case-sensitive)
+async function getAllBooksByAuthor(author) {
+    const booksCollection = await getBooksCollection();
+    const book = await booksCollection.findOne({ author });
+    return book;
 }
 
+// Function to get books by author (case-insensitive)
 async function getAllBooksByAuthorCaseInsensitive(author) {
-    let connection = await connect(url);
-    let db = connection.db('g7_cr_20240826');
-    let booksCollection = db.collection('books');
+    const booksCollection = await getBooksCollection();
     const book = await booksCollection.findOne({ author: new RegExp(author, 'i') });
     return book;
 }
 
-async function addNewBook(title,isbn,author,price,description,tags,cover,authorId,reviews){
-    let newBook = {title,isbn,author,price,description,tags,cover,authorId,reviews};
-    const connection = await connect(url);
-    const db = connection.db('g7_cr_20240826');
-    const booksCollection = db.collection('books');
+// Function to add a new book
+async function addNewBook(title, isbn, author, price, description, tags, cover, authorId, reviews) {
+    const newBook = { title, isbn, author, price, description, tags, cover, authorId, reviews };
+    const booksCollection = await getBooksCollection();
     const result = await booksCollection.insertOne(newBook);
     return result.insertedId;
 }
 
-async function deleteBookByTitle(title){
-    const connection = await connect(url);
-    const db = connection.db('g7_cr_20240826');
-    const booksCollection = db.collection('books');
-    const book =await booksCollection.deleteOne({title});
-    return "book with title \""+title+"\" deleted"
-}
-
-async function updateTitle(title,newTitle){
-    const connection = await connect(url);
-    const db = connection.db('g7_cr_20240826');
-    const booksCollection = db.collection('books');
-    const result = await booksCollection.updateMany({title},
-                                                    {$set : {title : newTitle}}
-                                                    );
-    if(result.matchedCount > 0){
-        return ("sucsessfully updates");
+// Function to delete a book by title
+async function deleteBookByTitle(title) {
+    const booksCollection = await getBooksCollection();
+    await booksCollection.deleteOne({ title });
+    let getBook = await booksCollection.findOne({title}).length;
+    if(getBook==0){
+        return `Book with title "${title}" deleted`;
     }else{
-        return ("No bok Found with iven title");
+        return `No book found with title ${title}`;
     }
 }
 
-async function getBooksByTitle(title){
-    //let booksCollection = connection();
-    const connection  = await connect(url);
-    const db = connection.db('g7_cr_20240826');
-    const booksCollection = db.collection('books');
-    let result =await booksCollection.findOne({title});
-    return result;
+// Function to update a book's title
+async function updateTitle(oldTitle, newTitle) {
+    const booksCollection = await getBooksCollection();
+    const result = await booksCollection.updateOne(
+        { title: oldTitle },
+        { $set: { title: newTitle } }
+    );
+    if (result.matchedCount > 0) {
+        return "Successfully updated the title.";
+    } else {
+        return "No book found with the given title.";
+    }
+}
+
+// Function to get a book by title
+async function getBooksByTitle(title) {
+    const booksCollection = await getBooksCollection();
+    const book = await booksCollection.findOne({ title });
+    return book;
 }
 
 module.exports = {
